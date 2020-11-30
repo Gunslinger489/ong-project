56a3cc9e00697dcbc672bb7a9ee1fc42236e340b
Henrique Marques <72282029+lhenriquedev@users.noreply.github.com>
2020-11-30 15:56:47 -0300

    animations

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 // Menu Responsivo
 let show = true;
 
-const menuSection = document.querySelector('.menu-section');
-const menuToggle = menuSection.querySelector('.menu-toggle');
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = menuSection.querySelector(".menu-toggle");
 
-menuToggle.addEventListener('click', () => {
-  menuSection.classList.toggle('on', show);
+menuToggle.addEventListener("click", () => {
+  menuSection.classList.toggle("on", show);
   show = !show;
 });
+
+ScrollReveal().reveal(".features", { delay: 500 });
+ScrollReveal().reveal(".about", { delay: 600 });
+ScrollReveal().reveal(".testimonials", { delay: 700 });
+ScrollReveal().reveal(".contact", { delay: 800 });
